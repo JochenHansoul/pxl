89e572b2a79b5a6e88536e0c50ab71c0621bd0fb
JochenHansoul <jochenhansoul101@hotmail.com>
2021-01-05 01:37:07 +0100

    just testing numberformatexceptions

---
+++ 2018/java essentials/chapters/01_intro/src/be/pxl/h1/eigenOefeningen/ParsingStrings.java
@@ -0,0 +1,9 @@
+package be.pxl.h1.eigenOefeningen;
+
+public class ParsingStrings {
+    public static void main(String[] args) {
+        String s = "hello word";
+        int i = Integer.parseInt(s);
+        System.out.println(i);
+    }
+}
