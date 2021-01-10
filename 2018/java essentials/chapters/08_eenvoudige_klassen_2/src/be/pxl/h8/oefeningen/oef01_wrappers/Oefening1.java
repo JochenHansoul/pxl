6e9cbe392ac7df21241fcf598e36c1f943f666fd
JochenHansoul <jochenhansoul101@hotmail.com>
2021-01-10 02:40:47 +0100

    Changed excercise to be more in line with the tast descriptions. Is now able to distinguis if a character is a letter, number or something else. Simplified the numbers before the point and numbers after the point for the double.

--- 2018/java essentials/chapters/08_eenvoudige_klassen_2/src/be/pxl/h8/oefeningen/oef01_wrappers/Oefening1.java
+++ 2018/java essentials/chapters/08_eenvoudige_klassen_2/src/be/pxl/h8/oefeningen/oef01_wrappers/Oefening1.java
@@ -20,42 +20,60 @@ public class Oefening1 {
         Character wrapKarakter = karakter;
         Integer wrapGetal = getal;
         Long wrapLangGetal = langGetal;
-        Double wrapDedimaalGetal = decimaalGetal;
+        Double wrapDecimaalGetal = decimaalGetal;
         // array test
         ArrayList<Object> test = new ArrayList<>();
-        test.add(wrapKarakter);
-        test.add(wrapGetal);
-        test.add(wrapLangGetal);
-        test.add(wrapDedimaalGetal);
-        System.out.println("long compare: " + Long.compare(wrapLangGetal, 0));
+        test.add(karakter);
+        test.add(getal);
+        test.add(langGetal);
+        test.add(decimaalGetal);
         for (Object o : test) {
-            System.out.print(o);
-            // character
-            if (o instanceof Character) {
-                if (Character.isLetter((Character) o)) {
-                    if (Character.isLowerCase((Character)o)){
-                        System.out.println(" is een kleine letter");
-                    } else {
-                        System.out.println(" is een grote letter");
-                    }
-                }
-            } else if (o instanceof Integer) {
-                System.out.println(" is een integer en de afstand tot het grootste is " + (Integer.MAX_VALUE - (Integer) o));
-            } else if (o instanceof Long) {
-                System.out.print(" is een long en de afstand tot het kleinste is ");
-                if (Long.compare((Long) o, 0) > 0) {
-                    System.out.println(Long.MAX_VALUE - (Long) o);
-                } else {
-                    System.out.println((Long) o - Long.MIN_VALUE);
-                }
-            } else if (o instanceof Double) {
-                System.out.println(" is een double en het aantal cijfers na de komma is "
-                        + (o.toString().length() - o.toString().indexOf(".") - 1)
-                        + " en voor de komma is " + o.toString().indexOf("."));
-            }
-            // iets anders
+            System.out.println(o);
         }
         Collections.shuffle(test);
+        for (Object o : test) {
+            printInfoAboutInstance(o);
+        }
         input.close();
+    }
+
+    private static void printInfoAboutInstance(Object object) {
+        if (object instanceof Character) {
+            char c = (char) object;
+            if (Character.isLetter(c)) {
+                if (Character.isLowerCase((Character) object)) {
+                    System.out.println(c +" is een kleine letter");
+                } else {
+                    System.out.println(c + " is een grote letter");
+                }
+            } else if (Character.isDigit(c)) {
+                System.out.println(c + " is een nummer");
+            } else {
+                System.out.println( c +" is een onbekend karakter");
+            }
+        } else if (object instanceof Integer) {
+            int i = (int) object;
+            if (i > 0) {
+                System.out.printf("%1$d is een int en de afstand tot het grootste getal is: %2$d%n",
+                        i, Integer.MAX_VALUE - i);
+            } else {
+                System.out.printf("%1$d is een int en de afstand tot het kleinste getal is: %2$d%n",
+                        i, i - Integer.MIN_VALUE);
+            }
+        } else if (object instanceof Long) {
+            long l = (long) object;
+            if (l > 0) {
+                System.out.printf("%1$d is een long en de afstand tot het grootste getal is: %2$d %n",
+                        l, Long.MAX_VALUE - l);
+            } else {
+                System.out.printf("%1$d is een long en de afstand tot het kleinste getal is: %d %n",
+                        l - Long.MIN_VALUE);
+            }
+        } else if (object instanceof Double) {
+            double d = (double) object;
+            String[] array = Double.toString(d).split("\\.");
+            System.out.printf("%1$f is een double en het aatal cijfers voor de komma is %2$d en na de komma is %3$d%n",
+                    d, array[0].length(), array[1].length());
+        }
     }
 }
