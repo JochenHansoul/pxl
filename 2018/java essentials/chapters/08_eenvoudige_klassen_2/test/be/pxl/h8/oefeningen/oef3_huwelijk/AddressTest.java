ca1ec89bc812256ffec8bc2a39324e2aea924ccb
JochenHansoul <jochenhansoul101@hotmail.com>
2021-01-12 17:49:54 +0100

    added tests for Address

---
+++ 2018/java essentials/chapters/08_eenvoudige_klassen_2/test/be/pxl/h8/oefeningen/oef3_huwelijk/AddressTest.java
@@ -0,0 +1,46 @@
+package be.pxl.h8.oefeningen.oef3_huwelijk;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AddressTest {
+
+    private Address address = new Address("straat", "1a", 1000, "Wellen");
+
+    @Test
+    public void testConstructorWithTownshipObject() {
+        Township township = new Township(1000, "Wellen");
+        Address address = new Address("straat", "1a", township);
+        assertEquals("straat", address.getStreet());
+        assertEquals("1a", address.getNumber());
+        assertEquals(township, address.getTownship());
+    }
+
+    @Test
+    public void testConstructorWithoutTownshipObject() {
+        Address address = new Address("straat", "1a", 1000, "Wellen");
+        assertEquals("straat", address.getStreet());
+        assertEquals("1a", address.getNumber());
+        assertEquals(1000, address.getTownship().getPostcode());
+        assertEquals("Wellen", address.getTownship().getName());
+    }
+
+    @Test
+    public void testChangeStreet() {
+        address.setStreet("new");
+        assertEquals("new", address.getStreet());
+    }
+
+    @Test
+    public void testChangeNumber() {
+        address.setNumber("2b");
+        assertEquals("2b", address.getNumber());
+    }
+
+    @Test
+    public void testToString() {
+        address = new Address("straat", "1a", 1000, "Wellen");
+        assertEquals("straat 1a\n1000 Wellen", address.toString());
+    }
+}
