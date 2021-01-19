92e7b9e9d470fe4a61178822f08fdd3dc0c0359d
JochenHansoul <jochenhansoul101@hotmail.com>
2021-01-19 07:35:42 +0100

    added a test with variables and difference between == === operator

---
+++ 2018/web scripting/tests/variables/adding.js
@@ -0,0 +1,21 @@
+"use strict"
+
+let i = 0
+let s = "hello"
+let x = parseInt("10")
+let y = Number("0")
+console.log(x)
+console.log(y)
+
+if (0 == "hello world") {
+    console.log("0 == hello world")
+} else {
+    console.log("0 != hello world")
+}
+
+if (0 == "") {
+    console.log("0 == \"\"")
+
+} else {
+    console.log("0 != \"\"")
+}
