b8e9ea1770a4790b661106d272ce00d59f92e53e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-02-03 20:25:22 +0100

    changed names of constant to make them more clearer

--- 2019/web advanced/phpstorm/tests/types/constants/simpleNamespace/source.php
+++ 2019/web advanced/phpstorm/tests/types/constants/simpleNamespace/source.php
@@ -19,21 +19,21 @@ function newFunction()
 newFunction();
 
 const NEW_CONSTANT = 10;
-const A = 10 + 1.1;
-const B = "a" . "b";
+const NUMBER = 10 + 1.1;
+const STRING_C = "a" . "b";
 const D = true + false;
 const E = 10 . "a";
-const F = 10.1 - A;
-const G = 10 . A;
-const H = G < A;
+const F = 10.1 - NUMBER;
+const G = 10 . NUMBER; // kan wel worden geconcateneerd met .
+const H = G < NUMBER;
 const I = H < 10;
-const J = H ?? A;
+const J = H ?? NUMBER;
 // strings en nummers kunnen niet opgeteld worden met const
 //const K = "a" + 5;
-//const L = B + A;
+//const L = STRING_C + NUMBER;
 
-echo gettype(A) . "\n";
-echo gettype(D);
+echo gettype(NUMBER) . "\n";
+echo gettype(D) . "\n";
 
 $var1 = 1;
 unset($var1);
