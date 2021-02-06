275c63b6eafe9ae789d426f7df50f593ae32099e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-02-06 14:35:54 +0100

    removed most of the uneccessary things

--- 2019/web advanced/phpstorm/chapters/06_mvc/01_codeigniter/public/index.php
+++ 2019/web advanced/phpstorm/chapters/06_mvc/01_codeigniter/public/index.php
@@ -1,45 +1,18 @@
 <?php
 
-// Valid PHP Version?
-$minPHPVersion = '7.2';
-if (phpversion() < $minPHPVersion)
-{
-	die("Your PHP version must be {$minPHPVersion} or higher to run CodeIgniter. Current version: " . phpversion());
-}
-unset($minPHPVersion);
+const MIN_PHP_VERSION = 7.2;
 
-// Path to the front controller (this file)
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
-
-// Location of the Paths config file.
 // This is the line that might need to be changed, depending on your folder structure.
-$pathsPath = realpath(FCPATH . '../app/Config/Paths.php');
-// ^^^ Change this if you move your application folder
+$pathConfigFile = dirname(__DIR__) . "/app/Config/Paths.php";
 
-/*
- *---------------------------------------------------------------
- * BOOTSTRAP THE APPLICATION
- *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
- */
-
-// Ensure the current directory is pointing to the front controller's directory
-chdir(__DIR__);
-
-// Load our paths config file
-require $pathsPath;
-$paths = new Config\Paths();
-
-// Location of the framework bootstrap file.
-$app = require rtrim($paths->systemDirectory, '/ ') . '/bootstrap.php';
-
-/*
- *---------------------------------------------------------------
- * LAUNCH THE APPLICATION
- *---------------------------------------------------------------
- * Now that everything is setup, it's time to actually fire
- * up the engines and make this app do its thang.
- */
-$app->run();
+if (phpversion() >= MIN_PHP_VERSION) {
+    // bootstrap and launch application
+    require_once $pathConfigFile;
+    chdir(__DIR__); // Ensure the current directory is pointing to the front controller's directory
+    $paths = new Config\Paths(); // this object is neccessary for som unknown reason
+    $app = require_once (new Config\Paths())->systemDirectory . "/bootstrap.php";
+    unset($paths); // but can be safely removed after the require_once
+    $app->run(); // launch application
+} else {
+    echo "Your PHP version must be " . MIN_PHP_VERSION . " or higher to run CodeIgniter. Current version: " . phpversion();
+}
