f7347c6324cb2a5836b3c6ce6485588a95935674
JochenHansoul <jochenhansoul101@hotmail.com>
2021-02-15 14:31:35 +0100

    cleaned up some last things

--- 2019/web advanced/phpstorm/chapters/06_mvc/01_codeigniter/public/index.php
+++ 2019/web advanced/phpstorm/chapters/06_mvc/01_codeigniter/public/index.php
@@ -3,18 +3,23 @@
 use Config\Paths;
 
 const MIN_PHP_VERSION = 7.2;
+// The next line might need to be changed, depending on your folder structure.
 const PATH_CONFIGURATION_FILE = "/app/Config/Paths.php";
-// This is the line that might need to be changed, depending on your folder structure.
 
-if (phpversion() >= MIN_PHP_VERSION) {
+$phpVersion = phpversion();
+
+if ($phpVersion >= MIN_PHP_VERSION) {
     // bootstrap and launch application
-    require_once dirname(__DIR__) . PATH_CONFIGURATION_FILE; 
-    chdir(__DIR__); // Ensure the current directory is pointing to the front controller's directory
-    $paths = new Paths(); // this object is neccessary for some unknown reason
-    //$app = require_once (new Paths())->systemDirectory . "/bootstrap.php";
-    $app = require_once "../vendor/codeigniter4/framework/system/bootstrap.php";
-    unset($paths); // but can be safely removed after the require_once statement
-    $app->run(); // launch application
+    require_once dirname(__DIR__) . PATH_CONFIGURATION_FILE;
+    // Ensure the current directory is pointing to the front controller's directory
+    chdir(__DIR__);
+    // $paths object needs to be created and may not be used to only require
+    // the bootstrap file
+    $paths = new Paths();
+    $app = require_once $paths->systemDirectory . "/bootstrap.php";
+    // launch application
+    $app->run();
 } else {
-    echo "Your PHP version must be " . MIN_PHP_VERSION . " or higher to run CodeIgniter. Current version: " . phpversion();
+    echo "Your PHP version must be " . MIN_PHP_VERSION
+        . " or higher to run CodeIgniter. Current version: {$phpVersion}";
 }
