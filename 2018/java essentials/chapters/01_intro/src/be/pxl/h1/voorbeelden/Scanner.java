891e4b8a55fb351d4b264fc5dd75bfc4264db736
JochenHansoul <jochenhansoul101@hotmail.com>
2021-04-17 14:40:45 +0200

    removed scanner class

--- 2018/java essentials/chapters/01_intro/src/be/pxl/h1/voorbeelden/Scanner.java
+++
@@ -1,8 +0,0 @@
-package be.pxl.h1.voorbeelden;
-
-public class Scanner {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner ();
-    /* een int en een string samen probeerd toe te voegen kunt ge het best onderscheiden door een nieuw line toe te voegen: scanner.nextLine(), zodat hij die mooi onderscheid */
-    }
-}
