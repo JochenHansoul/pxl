c8353dc80443a7149409a2759d7692041425d3c1
JochenHansoul <jochenhansoul101@hotmail.com>
2021-04-25 22:37:19 +0000

    just some tests in creating strings

--- 2018/java essentials/chapters/01_intro/src/be/pxl/h1/eigenOefeningen/strings/CreatingStrings.java
+++ 2018/java essentials/chapters/01_intro/src/be/pxl/h1/eigenOefeningen/strings/CreatingStrings.java
@@ -1,4 +1,19 @@
 package be.pxl.h1.eigenOefeningen.strings;
 
 public class CreatingStrings {
+    public static void main(String[] args) {
+        char[] charArray = new char[] {'s'};
+        String s1 = new String(new char[] {'s'});
+        String s2 = new String("s");
+        String s3 = "s";
+
+        Object o = "object";
+        System.out.println(o);
+        
+        System.out.println("before");
+        int[][] a2d = new int[][]{{1, 2}, {3, 4}};
+        System.out.println("after");
+
+
+    }
 }
