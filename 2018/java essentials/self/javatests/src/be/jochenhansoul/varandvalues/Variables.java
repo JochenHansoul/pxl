704a5d57fbd0804ead341c979292c23b6723a5da
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-13 00:32:04 +0200

    changed it to a test about a string value

--- 2018/java essentials/self/javatests/src/be/jochenhansoul/varandvalues/Variables.java
+++ 2018/java essentials/self/javatests/src/be/jochenhansoul/varandvalues/Variables.java
@@ -2,6 +2,7 @@ package be.jochenhansoul.varandvalues;
 
 public class Variables {
     public static void main(String[] arg) {
-        //
+        String s;
+        System.out.println("s".length());
     }
 }
