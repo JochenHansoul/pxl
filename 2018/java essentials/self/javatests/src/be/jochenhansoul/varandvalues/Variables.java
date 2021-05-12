3dca5744b3492ab71cdc3019a8726cd1af864aed
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-13 00:35:42 +0200

    added tust for empty string and string containing null

--- 2018/java essentials/self/javatests/src/be/jochenhansoul/varandvalues/Variables.java
+++ 2018/java essentials/self/javatests/src/be/jochenhansoul/varandvalues/Variables.java
@@ -2,7 +2,10 @@ package be.jochenhansoul.varandvalues;
 
 public class Variables {
     public static void main(String[] arg) {
-        String s;
         System.out.println("s".length());
+        String s;
+        //System.out.println(s.length()); // error: variable s might not have been initialized
+        s = null;
+        System.out.println(s.length()); // compiles but NullPointerException
     }
 }
