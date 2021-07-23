8a81c88801967e6a43cc8d17f3f799fccc73b55f
JochenHansoulIntec <jochen.hansoul@student.intecbrussel.be>
2021-07-23 14:03:56 +0200

    Organised the way to create variable variables.

--- 2019/web advanced/phpstorm/tests/variables/variableVariables.php
+++ 2019/web advanced/phpstorm/tests/variables/variableVariables.php
@@ -2,8 +2,27 @@
 
 declare(strict_types=1);
 
+// creating variable variables
 $var1 = "hello";
 $$var1 = "world";
 
+// printing
 echo "var1 and \$var1: $var1 ${$var1}\n";
 echo "hello: $hello\n";
+
+
+
+
+
+
+
+// even more stupid
+echo "\neven more stupid:\n";
+$specialVar = "stupid variable name";
+${$specialVar} = "wtf";
+//${can i do this} = "is this possible?"; // nope
+//${stupid1} = "is stupid";
+
+echo "stupid variable name: ${$specialVar}\n";
+//echo "stupid variable name: ${$tupid variable name}\n"; // syntax error
+//echo "stupid1: $stupid1\n";
