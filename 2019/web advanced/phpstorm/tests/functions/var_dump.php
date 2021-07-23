2c4d838b4f76b1cfdcb12695c51d48ff971e7e3d
JochenHansoulIntec <jochen.hansoul@student.intecbrussel.be>
2021-07-23 14:39:23 +0200

    Tested multiple variables with var_dump and tested more difference between var_export.

--- 2019/web advanced/phpstorm/tests/functions/var_dump.php
+++ 2019/web advanced/phpstorm/tests/functions/var_dump.php
@@ -3,3 +3,13 @@
 var_dump('' . true); // print de lengte van de string gevolgd door de string: "string(1) "1"
 //var_dump('' . true . '' . false . '' . true); // false wordt niet geprint
 //var_dump('' . true . '' . true . '' . true);
+
+// multiple variables
+echo "\nmultiple variables:\n";
+$var1 = "hello";
+$var2 = "world";
+var_dump($var1, $var2);
+
+echo "\nvar export:\n";
+//var_export($var1, $var2); // does only work with one variable
+var_export($var1); // also doenst print a new line
