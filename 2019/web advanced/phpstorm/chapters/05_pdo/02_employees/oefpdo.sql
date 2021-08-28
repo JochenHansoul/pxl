dca32dbf2b7a350a02c70856ccf4449d0f819e08
JochenHansoul <jochenhansoul101@hotmail.com>
2021-08-28 05:19:41 +0200

    Changed file from dos to unix newline and cleaned some things up.

--- 2019/web advanced/phpstorm/chapters/05_pdo/02_employees/oefpdo.sql
+++ 2019/web advanced/phpstorm/chapters/05_pdo/02_employees/oefpdo.sql
@@ -2,15 +2,17 @@ DROP DATABASE IF EXISTS oefpdo;
 
 CREATE DATABASE oefpdo;
 
+GRANT ALL PRIVILEGES ON oefpdo.* TO student@localhost;
+
 USE oefpdo;
 
-CREATE TABLE werknemers
-(
-   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   name varchar(30) NOT NULL
+CREATE TABLE user (
+    PRIMARY KEY (user_id),
+    user_id int         AUTO_INCREMENT,
+    name    varchar(30) NOT NULL
 );
 
-INSERT INTO werknemers (`id`, `name`) VALUES
-(1, 'Martin'),
-(2, 'Feynman'),
-(3, 'Graves');
+INSERT INTO user (user_id, name)
+VALUES  (1, 'Martin'),
+        (2, 'Feynman'),
+        (3, 'Graves');
