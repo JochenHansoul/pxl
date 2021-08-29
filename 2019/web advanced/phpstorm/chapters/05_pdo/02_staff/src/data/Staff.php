acac8135daefe729206d19a89a08bd5055551016
JochenHansoul <jochenhansoul101@hotmail.com>
2021-08-29 21:25:03 +0200

    Created Staff class

---
+++ 2019/web advanced/phpstorm/chapters/05_pdo/02_staff/src/data/Staff.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace data;
+
+class Staff
+{
+    private int $id;
+    private string $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function setName(String $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function __toString(): string
+    {
+        return "staff{id=" . $this->id . ",name=" . $this->name . "}";
+    }
+}
