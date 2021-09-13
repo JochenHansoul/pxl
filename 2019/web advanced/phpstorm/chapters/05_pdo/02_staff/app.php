533c8ca0e8cb8f456dd454282cd6c6a098b11db6
JochenHansoul <jochenhansoul101@hotmail.com>
2021-09-13 23:22:39 +0200

    Renamed staff to app, put a html root in this file and changed the paragraph of people to an organised list

---
+++ 2019/web advanced/phpstorm/chapters/05_pdo/02_staff/app.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use controller\StaffController;
+
+require_once "vendor/autoload.php";
+
+$staffController = new StaffController();
+$staffMembers = $staffController->index();
+
+// printing data
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>staff members</title>
+</head>
+<body>
+    <ol>
+    <?php foreach ($staffMembers as $staff) { ?>
+    <li><?php echo $staff->getName(); ?></li>
+    <?php } ?>
+    </ol>
+</body>
+</html>
