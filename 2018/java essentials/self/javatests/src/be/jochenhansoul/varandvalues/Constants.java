3f85006e3a58b18fd8cb6ee83999ce3a33f4d414
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-02 21:34:34 +0100

    Checked if constants can be used inside of a loop

---
+++ 2018/java essentials/self/javatests/src/be/jochenhansoul/varandvalues/Constants.java
@@ -0,0 +1,11 @@
+package be.jochenhansoul.varandvalues;
+
+public class Constants {
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+        for (int i = 0; i < 10; i++) {
+            final String I = "" + i;
+            System.out.println(I);
+        }
+    }
+}
