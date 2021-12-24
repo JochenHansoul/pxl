4419fa7fa08061a9dd3b103619c5f244e2c78013
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-24 19:49:35 +0100

    more tests but now with "test"

--- 2019/server os essentials/test/operator.sh
+++ 2019/server os essentials/test/operator.sh
@@ -1,5 +1,14 @@
 #!/bin/bash --
 
+#test
+BOOL2=$(test 0 -eq 0)
+echo $BOOL2
+test 0 -eq 0 && echo true || echo false
+[ $BOOL2 ] && echo true || echo false
+
+#[ ]
+echo ""
+echo "#[ ]:"
 BOOL=$([ 0 -eq 0 ])
 echo $BOOL
 [ 0 -eq 0 ] && echo true || echo false
