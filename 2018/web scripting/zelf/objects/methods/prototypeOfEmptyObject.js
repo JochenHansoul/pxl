7e99946c1ed3f3299502f0a21e18dfe131a36ad2
JochenHansoul <jochenhansoul101@hotmail.com>
2022-01-08 02:34:44 +0100

    Some Object.prototype tests

--- 2018/web scripting/zelf/objects/methods/prototypeOfEmptyObject.js
+++ 2018/web scripting/zelf/objects/methods/prototypeOfEmptyObject.js
@@ -1,6 +1,21 @@
 "use strict";
 
+console.log("object literal:");
+console.log({});
+// {}
+
+console.log("Object.getPrototypeOf({}):");
+console.log(Object.getPrototypeOf({}));
+// {}
+
+console.log("Object.prototype:");
+console.log(Object.prototype);
+// {}
+
+console.log("==:");
 console.log(Object.getPrototypeOf({}) == Object.prototype);
-// → true
-console.log(Object.getPrototypeOf(Object.prototype));
-// → null
+// true
+
+console.log("===:");
+console.log(Object.getPrototypeOf({}) === Object.prototype);
+// true
