6d0b7af62080353af7a03232e31f786976f6b310
JochenHansoul <jochen.hansoul@outlook.be>
2022-06-17 00:17:45 +0200

    Reformatted the html file and correctly identated the <?php tags!

--- 2019/web advanced/phpstorm/chapters/05_pdo/02_staff/app.php
+++ 2019/web advanced/phpstorm/chapters/05_pdo/02_staff/app.php
@@ -9,19 +9,18 @@ require_once "vendor/autoload.php";
 $staffController = new StaffController();
 $staffMembers = $staffController->index();
 
-// printing data
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>staff members</title>
+  <title>staff members</title>
 </head>
 <body>
-    <ol>
-    <?php foreach ($staffMembers as $staff): ?>
-        <li><?php echo $staff->getName(); ?></li>
-    <?php endforeach ?>
-    </ol>
+  <h1>staff members</h1>
+  <ul>
+<?php foreach ($staffMembers as $staff): ?>
+    <li><?php echo $staff->getName(); ?></li>
+<?php endforeach ?>
+  </ul>
 </body>
 </html>
