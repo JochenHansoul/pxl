53a4a111cc16eddce99fa62fba47f50f25a97617
JochenHansoul <jochen.hansoul@outlook.be>
2022-06-25 04:15:07 +0200

    Added tests for the StaffPDO class.

---
+++ 2019/web advanced/phpstorm/chapters/05_pdo/02_staff/tests/pdo/StaffPDOTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pdo;
+
+use data\Staff;
+use PHPUnit\Framework\TestCase;
+
+final class StaffPDOTest extends TestCase
+{
+    public function testClassConstructor(): void
+    {
+        $staffPDO;
+        try {
+            $staffPDO = new StaffPDO();
+        } catch (\PDOException $e) {
+            echo $e->message();
+        }
+        $this->assertNotNull($staffPDO);
+    }
+
+    public function testGetAllStaff(): void
+    {
+        $staffMembers = [];
+        try {
+            $staffPDO = new StaffPDO();
+            $staffMembers = $staffPDO->getAllStaff();
+        } catch (\PDOException $e) {
+            echo $e->message();
+        }
+        $this->assertSame(3, \count($staffMembers));
+
+        $staffMember = $staffMembers[0];
+        $this->assertSame(1, $staffMember->id);
+        $this->assertSame("Martin", $staffMember->nameFirst);
+        $this->assertSame("Freeman", $staffMember->nameLast);
+
+        $staffMember = $staffMembers[1];
+        $this->assertSame(2, $staffMember->id);
+        $this->assertSame("Feynman", $staffMember->nameFirst);
+        $this->assertSame("Tomson", $staffMember->nameLast);
+
+        $staffMember = $staffMembers[2];
+        $this->assertSame(3, $staffMember->id);
+        $this->assertSame("Graves", $staffMember->nameFirst);
+        $this->assertSame("Wonder", $staffMember->nameLast);
+    }
+}
