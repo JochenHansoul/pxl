ad31ca1900a399598d47bbef6399920420a00193
JochenHansoul <jochen.hansoul@outlook.be>
2022-06-25 03:27:34 +0200

    Added if not exists statements.

--- 2019/web advanced/phpstorm/chapters/05_pdo/02_staff/sql/oefpdo.sql
+++ 2019/web advanced/phpstorm/chapters/05_pdo/02_staff/sql/oefpdo.sql
@@ -1,12 +1,12 @@
--- deleting data
+-- DATABASE
 DROP DATABASE IF EXISTS oef_pdo;
 
+CREATE DATABASE IF NOT EXISTS oef_pdo;
+
+-- USER
 DROP USER IF EXISTS 'oef_pdo_user'@'localhost';
 
--- creating database
-CREATE DATABASE oef_pdo;
+CREATE USER IF NOT EXISTS 'oef_pdo_user'@'localhost' IDENTIFIED BY 'password';
 
--- creating user
-CREATE USER 'oef_pdo_user'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE ON oef_pdo.* TO 'oef_pdo_user'@'localhost';
 FLUSH PRIVILEGES;
